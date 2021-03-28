f655efecf947c160ba87ba4c7ab060d598e32a71
Wiktor Ziejewski <wiktor.ziejewski>
2021-03-28 23:51:25 +0200

    JavaBlog

---
+++ js/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function titleClickHandler(event){
+  const clickedElement = this;
+  console.log('Link was clicked!');
+
+  /* [DONE] remove class 'active' from all article links  */
+
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
+
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+    clickedElement.classList.add('active') 
+
+  /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts .active');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
+
+  /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector)
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+    
+  /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
